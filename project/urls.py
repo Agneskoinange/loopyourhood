a8fa8516b87f8a828a7f00112c9111aaf5c9f172
Agnes <koinageagness@gmail.com>
2022-06-18 16:04:14 +0300

    style templates

--- project/urls.py
+++ project/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('',include('app.urls')),
     path('accounts/register',user_views.register_user,name='register_user'),
     path('accounts/login',user_views.user_login,name='user_login'),
-    path('logout', user_views.user_logout, name='user_logout'),
+    path('logout',user_views.user_logout, name='user_logout'),
 ]
