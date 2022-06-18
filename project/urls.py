c57037081f456d5d47ae5d97e410176eafa3e917
Agnes <koinageagness@gmail.com>
2022-06-18 15:09:10 +0300

    configure pyuploadcare

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from app import views as hood_views
+from users import views as user_views
 
 
 urlpatterns = [
