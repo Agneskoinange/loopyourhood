80066e446fdcf6a5bf9943c63ad96a4665978a59
Agnes <koinageagness@gmail.com>
2022-06-18 15:28:48 +0300

    register models in admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import NeighbourHood,Business,Post,EmergencyService
 # Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(EmergencyService)
