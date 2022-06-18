10539d4788b966b26832bf0182bf99bb0d1e28ab
Agnes <koinageagness@gmail.com>
2022-06-18 16:07:23 +0300

    configure secret key

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n9pv63he$4hg=(gy4s1(cl#5)^8187ov3dfwo)4q5ne%^t#y#@'
+SECRET_KEY='loopyourhood1234'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
